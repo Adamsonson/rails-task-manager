73b51b8845e04a300eceb15901e0a9b98f9fb113
adam somora <adamsomora33@gmail.com>
2019-06-04 23:49:08 +0200

    Rails Task Manager exercise finally finished, all functionality working, just need to play with CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # list all tasks
+  get 'tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+
+  # show 1 task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
